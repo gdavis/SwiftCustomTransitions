176ff617a1fe708267a2c5610f9e5da0691f3c44
Grant Davis <grant.davis@gmail.com>
2015-04-07 10:37:27 -0600

    organization, cleanup unused code

--- SwiftCustomTransitions/CubeDetailViewController.swift
+++ SwiftCustomTransitions/CubeDetailViewController.swift
@@ -16,21 +16,4 @@ class CubeDetailViewController: UIViewController {
         self.view.layer.borderColor = UIColor.redColor().CGColor
         self.view.layer.borderWidth = 4.0
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
