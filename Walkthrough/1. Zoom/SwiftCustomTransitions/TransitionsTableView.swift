8cb8d76525c7e4891f182f8c8a2932653326b246
Grant Davis <grant.davis@gmail.com>
2015-04-14 14:32:31 -0600

    add structure for walkthrough, initial example

---
+++ Walkthrough/1. Zoom/SwiftCustomTransitions/TransitionsTableView.swift
@@ -0,0 +1,54 @@
+//
+//  TransitionsTableView.swift
+//  SwiftCustomTransitions
+//
+//  Created by Grant Davis on 2/27/15.
+//  Copyright (c) 2015 Grant Davis Interactive, LLC. All rights reserved.
+//
+
+import UIKit
+
+class TransitionsTableView: UITableViewController
+{
+    private enum ViewControllerIdentifiers : String
+    {
+        case detail = "detailViewController"
+        case popup = "popupViewController"
+        case cube = "cubeDetailViewController"
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
+    {
+        switch (indexPath.row) {
+        case 0:
+            
+            let detailVC = self.storyboard!.instantiateViewControllerWithIdentifier(ViewControllerIdentifiers.detail.rawValue) as! UIViewController
+            // TODO: configure view controller for custom transition
+            self.presentViewController(detailVC, animated: true, completion: nil)
+            
+            break
+            
+        case 1:
+            
+            let popupVC = self.storyboard!.instantiateViewControllerWithIdentifier(ViewControllerIdentifiers.popup.rawValue) as! UIViewController
+            self.presentViewController(popupVC, animated: true, completion: nil)
+            
+            break
+        
+        case 2:
+            
+            let detailVC = self.storyboard!.instantiateViewControllerWithIdentifier(ViewControllerIdentifiers.cube.rawValue) as! UIViewController
+            self.navigationController?.pushViewController(detailVC, animated: true)
+            
+            break
+            
+        default:
+            break
+        }
+        
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    
+    // $ct1
+}
